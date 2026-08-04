4f6fde33ec95d5280e26118ac044db75ab49a46b
Mojahidul Alam <148482250+Mojahidul21@users.noreply.github.com>
2026-08-04 06:10:43 +0600

    Create Polygon Area.cpp

---
+++ Theorem/Shoelace Formula/Polygon Area.cpp
@@ -0,0 +1,19 @@
+// Problem Title: Polygon Area
+// Problem Statement: https://cses.fi/problemset/task/2191/
+// Solution:
+
+#include <bits/stdc++.h>
+using namespace std;
+main() {
+    int n;
+    cin>>n;
+    vector<int>x(n),y(n);
+    for(int i{};i<n;++i)
+        cin>>x[i]>>y[i];
+
+    long long xy{};
+    for(int i{};i<n;++i)
+        xy+=(long long)x[i]*y[(i+1)%n]-(long long)y[i]*x[(i+1)%n];
+    
+    cout<<abs(xy);
+}
